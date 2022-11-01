9a49a8dc2259e5c7937aaff755c561625c4ad95b
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-01 12:24:53 +0300

    Model sınıfının hazırlanması

---
+++ app/src/main/java/com/hozanbaydu/adobe/Model.java
@@ -0,0 +1,13 @@
+package com.hozanbaydu.adobe;
+
+public class Model {
+    public String name;
+    public String password;
+
+
+    public Model(String name, String password) {
+        this.name = name;
+        this.password = password;
+
+    }
+}
