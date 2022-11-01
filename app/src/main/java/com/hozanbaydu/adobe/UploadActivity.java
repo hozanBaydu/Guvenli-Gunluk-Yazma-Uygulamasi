54f773d0f44fdee80ae24bcbe8780521efe5d95c
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-01 19:16:55 +0300

    Bilgilerin firebase yüklenmesi işlemlerinin bitirilmesi

--- app/src/main/java/com/hozanbaydu/adobe/UploadActivity.java
+++ app/src/main/java/com/hozanbaydu/adobe/UploadActivity.java
@@ -1,20 +1,80 @@
 package com.hozanbaydu.adobe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.hozanbaydu.adobe.databinding.ActivityMainBinding;
+import com.hozanbaydu.adobe.databinding.ActivityUploadBinding;
+
+import java.util.HashMap;
+
 
 public class UploadActivity extends AppCompatActivity {
+
+    private ActivityUploadBinding binding;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload);
+        binding = ActivityUploadBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 
 
     public void kaydetButton (View view){
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        String userEmail = firebaseUser.getEmail();
+
+        String nameText = binding.addNameText.getText().toString();
+        String passwordText = binding.addPasswordText.getText().toString();
+
+        HashMap<String, Object> postData = new HashMap<>();
+        postData.put("useremail",userEmail);
+        postData.put("name",nameText);
+        postData.put("password",passwordText);
+        postData.put("date", FieldValue.serverTimestamp());
+
+        firebaseFirestore.collection("Posts").add(postData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+
+                Intent intent = new Intent(UploadActivity.this, PasswordActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UploadActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
