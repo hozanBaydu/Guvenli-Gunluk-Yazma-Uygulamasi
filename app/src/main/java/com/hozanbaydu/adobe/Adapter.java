965a097625f047ae293e6a2ea4c859daf09a2998
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-01 13:21:23 +0300

    Adapter bitirilmesi

--- app/src/main/java/com/hozanbaydu/adobe/Adapter.java
+++ app/src/main/java/com/hozanbaydu/adobe/Adapter.java
@@ -1,4 +1,50 @@
 package com.hozanbaydu.adobe;
 
-public class Adapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hozanbaydu.adobe.databinding.RecyclerrowBinding;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.PostHolder> {
+
+    private ArrayList<Model> passwordArrayList;
+
+    public Adapter(ArrayList<Model> postArrayList) {
+        this.passwordArrayList = postArrayList;
+    }
+
+    class PostHolder extends RecyclerView.ViewHolder {
+        RecyclerrowBinding recyclerRowBinding;
+
+        public PostHolder(@NonNull RecyclerrowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerrowBinding recyclerRowBinding = RecyclerrowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PostHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+
+        holder.recyclerRowBinding.textView2.setText(passwordArrayList.get(position).name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return passwordArrayList.size();
+    }
+
+
 }
