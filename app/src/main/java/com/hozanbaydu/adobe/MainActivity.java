08410bfdcbdb48849b05f80f231e9d9bf3b26145
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-01 12:04:34 +0300

    giriş ve kayıt işlemleri

--- app/src/main/java/com/hozanbaydu/adobe/MainActivity.java
+++ app/src/main/java/com/hozanbaydu/adobe/MainActivity.java
@@ -1,26 +1,76 @@
 package com.hozanbaydu.adobe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.hozanbaydu.adobe.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
 
         getSupportActionBar().hide();
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 
     public void giris(View view){
 
+        String email = binding.emailText.getText().toString();
+        String password = binding.sifreText.getText().toString();
+
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(authResult -> {
+
+            Intent intent = new Intent(MainActivity.this, PasswordActivity.class);
+            startActivity(intent);
+            finish();
+        }).addOnFailureListener(e -> Toast.makeText(MainActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_SHORT).show());
     }
 
-    public void kayit(View view){
+    public void kayit (View view){
+
+        String email = binding.emailText.getText().toString();
+        String password = binding.sifreText.getText().toString();
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                Toast.makeText(MainActivity.this,"User Created",Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this,PasswordActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+
 
     }
 }
