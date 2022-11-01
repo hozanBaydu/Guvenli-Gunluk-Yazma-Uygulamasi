bfcc303724072c6537261c6196fd9b38b3730df1
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-01 19:07:55 +0300

    upload aktivitesi oluşturulması

---
+++ app/src/main/java/com/hozanbaydu/adobe/UploadActivity.java
@@ -0,0 +1,20 @@
+package com.hozanbaydu.adobe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+
+
+    public void kaydetButton (View view){
+
+    }
+}
