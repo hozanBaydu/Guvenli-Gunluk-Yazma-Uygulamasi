b514746ba81c98e482fc793238c24e53268ab5d1
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-01 12:17:04 +0300

    password aktivitesine viewbinding eklenmesi

--- app/src/main/java/com/hozanbaydu/adobe/PasswordActivity.java
+++ app/src/main/java/com/hozanbaydu/adobe/PasswordActivity.java
@@ -3,12 +3,30 @@ package com.hozanbaydu.adobe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+
+import com.hozanbaydu.adobe.databinding.ActivityMainBinding;
+import com.hozanbaydu.adobe.databinding.ActivityPasswordBinding;
+
 
 public class PasswordActivity extends AppCompatActivity {
+
+    private ActivityPasswordBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password);
+        binding = ActivityPasswordBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        getSupportActionBar().hide();
     }
+
+
+
+
+
 }
