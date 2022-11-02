84878e6be8162b741939ea0d59753e86a01e3379
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-02 13:52:40 +0300

    Verilerin alınıp kullanıcıya gösterilmesi işlemleri

--- app/src/main/java/com/hozanbaydu/adobe/PasswordActivity.java
+++ app/src/main/java/com/hozanbaydu/adobe/PasswordActivity.java
@@ -1,19 +1,37 @@
 package com.hozanbaydu.adobe;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.view.Display;
 import android.view.View;
+import android.widget.Toast;
 
-import com.hozanbaydu.adobe.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.hozanbaydu.adobe.databinding.ActivityPasswordBinding;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class PasswordActivity extends AppCompatActivity {
 
     private ActivityPasswordBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    ArrayList<Model> passwordArrayList;
+    Adapter RecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,19 @@ public class PasswordActivity extends AppCompatActivity {
 
 
         getSupportActionBar().hide();
+        passwordArrayList = new ArrayList<Model>();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+
+        getFromFirebase();
+
+        //RecyclerView
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerAdapter = new Adapter(passwordArrayList);
+        binding.recyclerView.setAdapter(RecyclerAdapter);
     }
 
 
@@ -34,5 +65,43 @@ public class PasswordActivity extends AppCompatActivity {
         finish();
     }
 
+    public void getFromFirebase(){
 
+        CollectionReference collectionReference = firebaseFirestore.collection("Posts");
+
+        collectionReference.orderBy("date", Query.Direction.DESCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if (e != null) {
+                    Toast.makeText(PasswordActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                }
+
+                if (queryDocumentSnapshots != null) {
+
+                    for (DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()) {
+
+                        Map<String,Object> data = snapshot.getData();
+
+                        //Casting
+                        String name = (String) data.get("name");
+                        String userEmail = (String) data.get("useremail");
+                        String password = (String) data.get("password");
+
+                        Model post = new Model(name,password,userEmail);
+
+                        passwordArrayList.add(post);
+
+                    }
+                    RecyclerAdapter.notifyDataSetChanged();
+
+                }
+
+            }
+        });
+
+
+    }
 }
+
+
