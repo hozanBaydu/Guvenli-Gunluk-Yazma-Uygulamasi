eeb65d3501d60b09fc46948b12825c3308b98ceb
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-11-02 13:51:48 +0300

    Modelin serileştirilmesi

--- app/src/main/java/com/hozanbaydu/adobe/Model.java
+++ app/src/main/java/com/hozanbaydu/adobe/Model.java
@@ -1,13 +1,17 @@
 package com.hozanbaydu.adobe;
 
-public class Model {
+import java.io.Serializable;
+
+public class Model implements Serializable {
     public String name;
     public String password;
+    public String email;
 
 
-    public Model(String name, String password) {
+    public Model(String name, String password,String email) {
         this.name = name;
         this.password = password;
+        this.email=email;
 
     }
 }
